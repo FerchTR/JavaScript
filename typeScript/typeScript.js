8a535fab81835c9b302b4bb0c602e85c4f55da83
Edison <eddyferch@hotmail.es>
2018-04-25 12:20:42 -0500

    typescript

--- typeScript/typeScript.js
+++ typeScript/typeScript.js
@@ -1,5 +1,3 @@
 //lengaje de typeScript hace transpilar, tranforma un lenguaje de programacion a otro. hey que decir a typeScript a que version transpilar
-
-
-console.log('hola mundo')
+console.log('hola mundo');
 var hola;
